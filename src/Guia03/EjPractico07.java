ecc8debbe6d7b0fb85b0d317ab25018f0d236b31
Ana Maria Dieguez <amdieguez2003@yahoo.com.ar>
2023-03-26 17:40:57 -0300

    Agregando ejercicio 07 guia 03

--- src/Guia03/EjPractico07.java
+++ src/Guia03/EjPractico07.java
@@ -3,6 +3,7 @@ package Guia03;
 import java.util.Scanner;
 
 public class EjPractico07 {
+
     public static void main(String[] args) {
         /* 7. Realizar un programa que simule el funcionamiento de un dispositivo
         RS232, este tipo de dispositivo lee cadenas enviadas por el usuario. Las
@@ -17,8 +18,33 @@ public class EjPractico07 {
         lecturas correctas e incorrectas recibidas. Para resolver el ejercicio
         deberá investigar cómo se utilizan las siguientes funciones de Java
         Substring(), Length(), equals().
-        */
+         */
         Scanner leer = new Scanner(System.in);
-        
+        String cadena = "null";
+        int sumCorrecta = 0;
+        int sumIncorrecta = 0;
+        int cantidad;
+        String carI = "null";
+        String carF = "null";
+        String FDE = "&&&&&";
+        do {
+            System.out.println("Ingrese una cadena");
+            cadena = leer.nextLine();
+            cantidad = cadena.length();
+            carI = cadena.substring(0, 1);
+            carF = cadena.substring(cantidad - 1, cantidad);
+            if (cantidad <= 5 && carI.equalsIgnoreCase("X") && carF.equalsIgnoreCase("O")) {
+                sumCorrecta = sumCorrecta + 1;
+            } else {
+                if (cadena.equals(FDE)) {
+                    break;
+                } else {
+                    sumIncorrecta = sumIncorrecta + 1;
+                }
+            }
+        } while (!(cadena.equals(FDE)));
+
+        System.out.println("La cantidad de cadenas Correctas fue: " + sumCorrecta);
+        System.out.println("La cantidad de cadenas Incorrectas fue: " + sumIncorrecta);
     }
 }
