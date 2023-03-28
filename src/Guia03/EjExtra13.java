ace1359ddf2148dabfb2c5a6b1f073364dd7d059
Ana Maria Dieguez <amdieguez2003@yahoo.com.ar>
2023-03-28 14:35:38 -0300

    Agregando Ejercicio Extra 13

--- src/Guia03/EjExtra13.java
+++ src/Guia03/EjExtra13.java
@@ -3,7 +3,7 @@ package Guia03;
 import java.util.Scanner;
 
 public class EjExtra13 {
-       
+
     public static void main(String[] args) {
         /* 13.Crear un programa que dibuje una escalera de números, donde cada
         línea de números comience en uno y termine en el número de la línea.
@@ -11,7 +11,16 @@ public class EjExtra13 {
         ingresa el número 3:
         1 12
         123 
-        */
+         */
         Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la altura de la escalera");
+        int escalon = leer.nextInt();
+        int cont = 0;
+        for (int i = 1; i <= escalon; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
     }
 }
